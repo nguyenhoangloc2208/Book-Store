51b452464dd7a3b72b0d45308214c7e8df56ad88
nguyenhoangloc2208 <nguyenhoangloc2208@gmail.com>
2024-04-18 04:24:51 +0700

    z

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000'
+export const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/'
 export const REACT_APP_STRIPE_KEY = process.env.REACT_APP_STRIPE_KEY || 'pk_test_51OBJ1aJbhG9XeLpgozBoW7WSsFdRLOa9oIs16k9sxDypcYCpKkV2bJm4H7DlvJFphgMAKsb1ZVyNQmRfuz9eS9ur00OcprV9xJ'
